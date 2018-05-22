219c5e1a12885a9f2837f0fcb57768a0f89f367e
Christian Bourlier <chrisbourlier@hotmail.com>
2018-05-22 12:26:45 -0600

    initial default state with all components rendering

--- src/App.js
+++ src/App.js
@@ -1,18 +1,22 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import MessageList from './components/message_list'
+import React, { Component } from 'react'
+import ToolBar from './components/toolbar'
+import './index.css'
+import messageData from './messageData.json'
+import ComposeForm from './components/compose_form'
 
 class App extends Component {
+  constructor (props) {
+    super(props)
+    this.state = { messages: messageData }
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <ToolBar messages={this.state.messages}/>
+        <ComposeForm />
+        <MessageList messages={this.state.messages}/>
       </div>
     );
   }
---
+++ src/components/toolbar.js
@@ -0,0 +1,67 @@
+import React, { Component } from 'react'
+import '../index.css'
+
+class ToolBar extends Component {
+  constructor(props) {
+    super(props)
+
+    const unreadMessages = props.messages.filter(e => e.read === false).length
+    this.state = {
+      unreadMessages: unreadMessages
+    }
+  }
+
+  countUnreadMessages = () => {
+
+  }
+
+  render() {
+    return (
+      <div className="row toolbar">
+        <div className="col-md-12">
+          <p className="pull-right">
+            <span className="badge badge">{this.state.unreadMessages}</span>
+            unread messages
+          </p>
+
+          <a className="btn btn-danger">
+            <i className="fa fa-plus"></i>
+          </a>
+
+          <button className="btn btn-default disabled">
+            <i className="fa fa-check-square-o"></i>
+          </button>
+
+          <button className="btn btn-default disabled">
+            Mark As Read
+          </button>
+
+          <button className="btn btn-default disabled">
+            Mark As Unread
+          </button>
+
+          <select className="form-control label-select">
+            <option>Apply label</option>
+            <option value="dev">dev</option>
+            <option value="personal">personal</option>
+            <option value="gschool">gschool</option>
+          </select>
+
+          <select className="form-control label-select">
+            <option>Remove label</option>
+            <option value="dev">dev</option>
+            <option value="personal">personal</option>
+            <option value="gschool">gschool</option>
+          </select>
+
+          <button className="btn btn-defalut">
+            <i className="fa fa-trash-o"></i>
+          </button>
+
+        </div>
+      </div>
+    )
+  }
+}
+
+export default ToolBar
